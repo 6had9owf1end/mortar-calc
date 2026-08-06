c4e27e8bdc8035b1823938d16e325aac863a827b
6had9owf1end <127658971+6had9owf1end@users.noreply.github.com>
2026-08-06 07:38:10 +0300

    Update service worker cache version and add listener
    Updated CACHE_NAME to version 4 and added message event listener for SKIP_WAITING.

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-const CACHE_NAME = 'mortar-calc-v1';
+const CACHE_NAME = 'mortar-calc-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
@@ -11,6 +10,10 @@ self.addEventListener('activate', (e) => {
     caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
   self.clients.claim();
+});
+
+self.addEventListener('message', (e) => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
