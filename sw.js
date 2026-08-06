34e35137141cf7f355e2bcec0ba6bd6272336f7a
6had9owf1end <127658971+6had9owf1end@users.noreply.github.com>
2026-08-06 13:34:27 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mortar-calc-v5';
+const CACHE_NAME = 'mortar-calc-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', (e) => {
